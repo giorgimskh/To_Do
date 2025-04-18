e12d09af317eb92f840886193defbe3b56c8465a
giorgimskh <giorgimsxrikadze@gmail.com>
2025-04-19 02:32:47 +0400

    Creating Model for data holding

---
+++ src/src/todo/model/Folder.java
@@ -0,0 +1,26 @@
+package todo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Folder<T> {
+    private List<Task> Tasks;
+    private String Name;
+
+    public Folder(List<Task> tasks, String name) {
+        Tasks = tasks;
+        Name = name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public List<Task> getTasks() {
+        return Tasks;
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
---
+++ src/src/todo/model/Task.java
@@ -0,0 +1,32 @@
+package todo.model;
+
+import java.time.LocalDate;
+
+public class Task {
+    private String description;
+    private boolean completed;
+    private LocalDate DueTime;
+
+    public Task(String description, boolean completed, LocalDate dueTime) {
+        this.description = description;
+        this.completed = completed;
+        DueTime = dueTime;
+    }
+
+    public Task(String description, boolean completed) {
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDate getDueTime() {
+        return DueTime;
+    }
+}
